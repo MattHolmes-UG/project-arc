396b9d9ce4c55524c5ecbc7a70907e02c10af32a
Mattholmes-ug <matthewochonogorug@gmail.com>
2021-09-28 18:59:28 +0100

    basic set up

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '92fc41ef94674c4fc6a141fa81084fc1'),
+    },
+  },
+});
